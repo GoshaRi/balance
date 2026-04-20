6c366bec960971eb1d0189f6d61483b0671cc083
GoshaRi <fisshrio@gmail.com>
2026-04-21 00:32:04 +0300

    del upd

--- js/ui.js
+++ js/ui.js
@@ -141,7 +141,7 @@ function renderHistory(transactions, editingId) {
                             ${t.type === 'income' ? '+' : '-'}${formatMoney(t.amount).replace(' ₽', '')} ₽
                         </span>
                     </div>
-                    <button class="delete-btn" onclick="window.handleDelete(${t.id}); event.stopPropagation();" title="Удалить">
+                    <button class="delete-btn" onclick="event.stopPropagation(); window.handleDelete(${t.id})" title="Удалить">
                         <svg viewBox="0 0 24 24"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                     </button>
                 </div>
